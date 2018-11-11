b76e106cbeaa17e75e1032583a62990c2fcd0723
Michal Zalecki <michal@michalzalecki.com>
2018-11-11 09:02:55 +0100

    Respond to new tweets

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,10 +1,22 @@
+import axios from "axios";
+const Sentiment = require("sentiment");
 const Twitter = require("twitter");
 
 interface Tweet {
   text: string;
+  id_str: string;
+  user: {
+    screen_name: string;
+  }
 }
 
-const tw = require("./tweet.json");
+interface Responses {
+  [key: string]: { matches: RegExp, responses: string[] };
+}
+
+// const tw = require("./tweet.json");
+const responses: Responses = require("./responses");
+const sentiment = new Sentiment();
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -13,13 +25,13 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-function answer(status: string, inReplyToStatusId?: number) {
+function answer(status: string, inReplyToStatusId?: number | string) {
   const reply = inReplyToStatusId
     ? { in_reply_to_status_id: inReplyToStatusId, auto_populate_reply_metadata: true }
     : {};
 
   client.post("statuses/update", { status, ...reply }, (_error: any, tweet: Tweet, _response: any) => {
-    console.log(tweet);
+    console.log(`Created tweet: ${tweetUrl(tweet)}`);
   });
 }
 
@@ -27,16 +39,76 @@ function answer(status: string, inReplyToStatusId?: number) {
 
 function search() {
   client.get("search/tweets", { q: "@MichalZalecki" }, function(_error: any, tweets: { statuses: Tweet[] }, _response: any) {
-    tweets.statuses.forEach(tweet => console.log(tweet.text));
+    tweets.statuses.forEach((tweet) => {
+      console.log(`Previous tweet: ${tweetUrl(tweet)}`)
+    });
   });
 }
 
-// search();
+search();
+
+/*
+ * Streaming
+ */
+
+function random(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomElement(arr: any[]) {
+  return arr[random(0, arr.length - 1)];
+}
+
+
+function tweetUrl(tweet: Tweet) {
+  return `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+}
+
+function getMatchingResponse(keywords: string[]) {
+  const result = Object.values(responses).find(({ matches }) => {
+    return !!(keywords.find(k => !!k.match(matches)));
+  });
+
+  return result ? randomElement(result.responses) : null;
+}
+
+async function getTweetKeywords(tweet: Tweet) {
+  const url = `https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases`;
+  const headers = { "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY };
+  const res = await axios.post<{ documents: { id: string, keyPhrases: string[] }[] }>(
+    url,
+    { documents: [{ id: tweet.id_str, text: tweet.text }] },
+    { headers }
+  );
+
+  return res.data.documents[0].keyPhrases;
+}
+
+async function getTweetSentiment(tweet: Tweet) {
+  return sentiment.analyze(tweet.text).comparative;
+}
+
+// getTweetSentiment(tw);
+
+async function handleIncomingTweet(tweet: Tweet) {
+  const keywords = await getTweetKeywords(tweet);
+  const response = await getTweetSentiment(tweet) < 0
+    ? getMatchingResponse(keywords)
+    : "Thank you! We're glad that you enjoy traveling with Eurostar!";
+
+  if (response) {
+    answer(response, tweet.id_str);
+  }
+}
+
+// handleIncomingTweet(tw);
 
 function stream() {
   client.stream("statuses/filter", { track: "@MichalZalecki" }, function(stream: any) {
     stream.on("data", function(tweet: Tweet) {
-      console.log(tweet);
+      console.log(`Incoming tweet: ${tweetUrl(tweet)}`);
+
+      handleIncomingTweet(tweet);
     });
 
     stream.on("error", function(error: Error) {
@@ -45,4 +117,4 @@ function stream() {
   });
 }
 
-// stream();
+stream();
